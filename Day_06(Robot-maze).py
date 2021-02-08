b19c7b1fd1eda8fee99178ecedc04d1e6aedd1f8
Rainbow <bowrainradio@gmail.com>
2021-02-08 12:14:53 +0100

    Day_06_Project
    Some fun using robot.abiedukacja.eu website.

---
+++ Day_06(Robot-maze).py
@@ -0,0 +1,44 @@
+#Some fun using http://robot.abixedukacja.eu/?lang=en&mode=python
+
+# Hurdle 4 Jump random walls
+
+# def turn_right():
+#     for x in range(3):
+#         turn_left()
+#
+# def jump_wall():
+#      turn_left()
+#      while wall_on_right():
+#          move()
+#      turn_right()
+#      move()
+#      turn_right()
+#      while front_is_clear():
+#          move()
+#      turn_left()
+#
+# while not at_goal():
+#      if front_is_clear():
+#          move()
+#      elif wall_in_front():
+#          jump_wall()
+#      else:
+#          move()
+
+
+
+
+#Maze It will find the way... hopefully
+
+# def turn_right():
+#     for x in range(3):
+#         turn_left()
+#
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
